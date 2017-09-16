5702caf0f3fa2138b0b49cadb6ea234420c5f4d2
Leon <Strikerklm96@gmail.com>
2017-09-15 23:57:37 -0500

    Loads MNIST Data;

--- NeuralNetworkTest/src/main.cpp
+++ NeuralNetworkTest/src/main.cpp
@@ -1,15 +1,27 @@
 
 #include "stdafx.hpp"
+#include "mnist/mnist_reader_less.hpp"
 
 using namespace std;
 using namespace Eigen;
 
-
-
-
-
 int main(int argc, char* argv[])
-{
+{	
+
+    //// Load MNIST data
+    //mnist::MNIST_dataset<std::vector, std::vector<uint8_t>, uint8_t> dataset = mnist::read_dataset<std::vector, std::vector, uint8_t, uint8_t>(MNIST_DATA_LOCATION);
+
+	mnist::filePath = File::getContentDir() + "mnist/";
+	auto dataset = mnist::read_dataset();
+
+    std::cout << "Nbr of training images = " << dataset.training_images.size() << std::endl;
+    std::cout << "Nbr of training labels = " << dataset.training_labels.size() << std::endl;
+    std::cout << "Nbr of test images = " << dataset.test_images.size() << std::endl;
+    std::cout << "Nbr of test labels = " << dataset.test_labels.size() << std::endl;
+
+
+
+
 	VectorXf greyImage(Constants::imageSize());
 
 	File::loadPngAsGrayscale("test.png", &greyImage);
